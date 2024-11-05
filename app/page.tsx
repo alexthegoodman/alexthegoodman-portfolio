11703af13a9e507bcbcd25117b231148af578feb
Alex Goodman <alexthegoodman@gmail.com>
2024-11-05 09:01:05 -0500

    improve pcb home

--- app/page.tsx
+++ app/page.tsx
@@ -24,11 +24,38 @@ export default function Home() {
             </p>
             <p>
               Now, I'm excited to leverage my talents to design and layout
-              circuits, and I am actively seeking great mentors.
+              circuits and am actively seeking mentors.
             </p>
           </div>
           <div className={styles.right}>
             <img src="/img/pcb/board2.png" />
+          </div>
+        </div>
+      </section>
+      <section className={styles.pcbInfo}>
+        <div className={styles.contain}>
+          <div className={styles.top}>
+            <h4>Continuing to Grow</h4>
+            <ul>
+              <li>
+                Basic component selection, research, and datasheet analysis
+              </li>
+              <li>Multi-layer board layouts</li>
+              <li>Calculating between units, calculating values</li>
+              <li>Knowledge of digital logic and analog fundamentals</li>
+            </ul>
+          </div>
+          <div className={styles.bottom}>
+            <img src="/img/pcb/board1_horiz.png" />
+          </div>
+        </div>
+      </section>
+      <section className={styles.gallery}>
+        <div className={styles.contain}>
+          <h3>Schema Gallery</h3>
+          <div className={styles.grid}>
+            <img src="/img/pcb/schema1.png" />
+            <img src="/img/pcb/schema2.png" />
           </div>
         </div>
       </section>
