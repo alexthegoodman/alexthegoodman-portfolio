f8ded8e52b704fb6e7a4e2d320bb06d16a709b54
Alex Woodman <alexthegoodman@gmail.com>
2025-03-15 08:20:23 -0400

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./page.module.scss";
 
 import Link from "next/link";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRight,
+  faCode,
+  faAd,
+  faRadio,
+} from "@fortawesome/free-solid-svg-icons";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
@@ -10,52 +15,22 @@ export default function Home() {
   return (
     <>
       <Header />
-      <section className={styles.pcbHero}>
+      <section className={styles.launchHero}>
         <div className={styles.contain}>
-          <div className={styles.left}>
-            <h1>
-              {"Hi, I'm"} <br />
-              {"Alex Woodman"}
-            </h1>
-            <h2>PCB Designer</h2>
-            <p>
-              From 10+ years as a software developer, I gained an understanding
-              for logic, mathematics, and module selection.
-            </p>
-            <p>
-              {`Now, I'm excited to leverage my talents to design and layout
-              circuits and am actively seeking mentors.`}
-            </p>
-          </div>
-          <div className={styles.right}>
-            <img src="/img/pcb/board2.png" />
-          </div>
-        </div>
-      </section>
-      <section className={styles.pcbInfo}>
-        <div className={styles.contain}>
-          <div className={styles.top}>
-            <h4>Continuing to Grow</h4>
-            <ul>
-              <li>
-                Basic component selection, research, and datasheet analysis
-              </li>
-              <li>Multi-layer board layouts</li>
-              <li>Calculating between units, calculating values</li>
-              <li>Knowledge of digital logic and analog fundamentals</li>
-            </ul>
-          </div>
-          <div className={styles.bottom}>
-            <img src="/img/pcb/board1_horiz.png" />
-          </div>
-        </div>
-      </section>
-      <section className={styles.gallery}>
-        <div className={styles.contain}>
-          <h3>Schema Gallery</h3>
-          <div className={styles.grid}>
-            <img src="/img/pcb/schema1.png" />
-            <img src="/img/pcb/schema2.png" />
+          <h1>What are you looking for?</h1>
+          <div className={styles.features}>
+            <Link href="/development" className={styles.feature}>
+              <FontAwesomeIcon icon={faCode} width="45px" />
+              <span>Software Engineering / Web Development</span>
+            </Link>
+            <Link href="/marketing" className={styles.feature}>
+              <FontAwesomeIcon icon={faAd} width="45px" />
+              <span>Digital Marketing</span>
+            </Link>
+            <Link href="/pcb-design" className={styles.feature}>
+              <FontAwesomeIcon icon={faRadio} width="45px" />
+              <span>PCB Design</span>
+            </Link>
           </div>
         </div>
       </section>
---
+++ app/pcb-design/page.tsx
@@ -0,0 +1,65 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "../page.module.scss";
+
+import Link from "next/link";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <section className={styles.pcbHero}>
+        <div className={styles.contain}>
+          <div className={styles.left}>
+            <h1>
+              {"Hi, I'm"} <br />
+              {"Alex Woodman"}
+            </h1>
+            <h2>PCB Designer</h2>
+            <p>
+              From 10+ years as a software developer, I gained an understanding
+              for logic, mathematics, and module selection.
+            </p>
+            <p>
+              {`Now, I'm excited to leverage my talents to design and layout
+              circuits and am actively seeking mentors.`}
+            </p>
+          </div>
+          <div className={styles.right}>
+            <img src="/img/pcb/board2.png" />
+          </div>
+        </div>
+      </section>
+      <section className={styles.pcbInfo}>
+        <div className={styles.contain}>
+          <div className={styles.top}>
+            <h4>Continuing to Grow</h4>
+            <ul>
+              <li>
+                Basic component selection, research, and datasheet analysis
+              </li>
+              <li>Multi-layer board layouts</li>
+              <li>Calculating between units, calculating values</li>
+              <li>Knowledge of digital logic and analog fundamentals</li>
+            </ul>
+          </div>
+          <div className={styles.bottom}>
+            <img src="/img/pcb/board1_horiz.png" />
+          </div>
+        </div>
+      </section>
+      <section className={styles.gallery}>
+        <div className={styles.contain}>
+          <h3>Schema Gallery</h3>
+          <div className={styles.grid}>
+            <img src="/img/pcb/schema1.png" />
+            <img src="/img/pcb/schema2.png" />
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
